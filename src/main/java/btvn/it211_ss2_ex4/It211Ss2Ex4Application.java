dd01b085e74c035edec4082667e402066059d0e7
Thanh <thenngk6@gmail.com>
2026-05-17 23:22:03 +0700

    Add files via upload

---
+++ src/main/java/btvn/it211_ss2_ex4/It211Ss2Ex4Application.java
@@ -0,0 +1,13 @@
+package btvn.it211_ss2_ex4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class It211Ss2Ex4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(It211Ss2Ex4Application.class, args);
+    }
+
+}
---
+++ src/test/java/btvn/it211_ss2_ex4/It211Ss2Ex4ApplicationTests.java
@@ -0,0 +1,13 @@
+package btvn.it211_ss2_ex4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class It211Ss2Ex4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
